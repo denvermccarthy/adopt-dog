c36794f69cbdb4ba79402f7cd966e2d65b26a43e
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-09 12:17:56 -0800

    displayed dogs

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ const dogListContainer = document.getElementById('dog-list-container');
 
 // on load
 // fetch all dogs
+window.addEventListener('load', async() => {
+    const dogs = await getDogs();
+    for (const dog of dogs) {
+        dogListContainer.append(renderDogCard(dog));
+    }
+
+});
 // render and append all dog cards to the container
