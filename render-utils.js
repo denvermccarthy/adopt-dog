ed24e80e2accf3542d5ebd0bf41071c9b0e51d0a
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-09 14:48:39 -0800

    fixed eslint

--- render-utils.js
+++ render-utils.js
@@ -8,7 +8,7 @@ export function renderDogCard(dog) {
 
     p.textContent = dog.name;
     img.src = `./assets/${dog.breed}.jpeg`;
-    a.href = `/detail/?id=${dog.id}`;
+    a.href = `./detail/?id=${dog.id}`;
     
     div.append(p, img);
 
